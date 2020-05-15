80fb542c2a3996fbf5b0d5f9b9a261cc2cc98772
jdrestre <jdrestre79@gmail.com>
2020-05-15 18:56:35 -0500

    First attemp to solved Task 2 web dynamic

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+const $ = window.$;
+$(document).ready(function () {
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+});
+jqte
+$(seletor).text(textString);
+
+$(selector).attr(attributeName);
