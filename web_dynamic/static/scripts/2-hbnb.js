ea4c3a39d643e9c69d79134e14c0d123ef7464a5
Simonster04 <simon.pahe@gmail.com>
2020-05-15 22:17:47 -0500

    Adding task 3, about knowing the status whe requesting the HBNB API

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+const $ = window.$;
+$(document).ready(function () {
+  const amenities = {};
+  $('INPUT[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
+$(seletor).text(textString);
+$(selector).attr(attributeName);
